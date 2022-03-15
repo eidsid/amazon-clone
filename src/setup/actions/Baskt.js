fb00f8a2a634d7e81abe017e5639da948ccbbaf3
eid sid <eidsiddiedis@gmail.com>
2022-03-15 23:49:08 +0200

    remove cod

--- src/setup/actions/Baskt.js
+++ src/setup/actions/Baskt.js
@@ -7,12 +7,6 @@ import {
 } from "firebase/firestore/lite";
 import { db } from "../../setup/firbase";
 
-export const createDBUser = (userDetails, id) => async (dispatch) => {
-  const usersRef = collection(db, `users/${id}/info`);
-  const userinfoRef = doc(usersRef, "userDetails");
-  await setDoc(userinfoRef, userDetails);
-};
-
 export const getItems = (id) => async (dispatch) => {
   try {
     let data = [];
@@ -30,8 +24,8 @@ export const getItems = (id) => async (dispatch) => {
 };
 export const ADD_ITEM = (userID, item) => async (dispatch) => {
   try {
-    const orderRef = collection(db, `users/${userID}/Baskt`);
-    const userinfoRef = doc(orderRef, `${item.id}`);
+    const BasktRef = collection(db, `users/${userID}/Baskt`);
+    const userinfoRef = doc(BasktRef, `${item.id}`);
     setDoc(userinfoRef, item).then(() => {
       dispatch({ type: "ADD_ITEM", payload: item });
     });
@@ -42,6 +36,7 @@ export const ADD_ITEM = (userID, item) => async (dispatch) => {
 export const REMOVE_ITEM = (id) => async (dispatch) => {
   try {
     // const { data } = api.createItem(item);
+
     console.log(id);
     dispatch({ type: "REMOVE_ITEM", payload: id });
   } catch (error) {

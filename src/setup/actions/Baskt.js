2557fa4ef2c39986e02b6e4284912146330d798c
eid sid <eidsiddiedis@gmail.com>
2022-03-15 23:06:51 +0200

    working on Baskt component

--- src/setup/actions/Baskt.js
+++ src/setup/actions/Baskt.js
@@ -1,7 +1,24 @@
-export const getItems = () => async (dispatch) => {
+import {
+  doc,
+  setDoc,
+  getDocs,
+  getDoc,
+  collection,
+} from "firebase/firestore/lite";
+import { db } from "../../setup/firbase";
+
+export const createDBUser = (userDetails, id) => async (dispatch) => {
+  const usersRef = collection(db, `users/${id}/info`);
+  const userinfoRef = doc(usersRef, "userDetails");
+  await setDoc(userinfoRef, userDetails);
+};
+
+export const getItems = (id) => async (dispatch) => {
+  console.log(id);
   try {
-    // const { data } = await api.getItems();
-    const data = [];
+    const userinfoRef = doc(db, "users", `${id}/info/Bakst`);
+    const data = await getDocs(userinfoRef);
+
     dispatch({ type: "FETCH_ALL", payload: data });
   } catch (error) {
     console.log(error.message);

ef4ed153eb4dc4923506e8b7737b0726c5f54867
eid sid <eidsiddiedis@gmail.com>
2022-03-15 22:41:25 +0200

    set up user Details with firebase

--- src/setup/actions/user.js
+++ src/setup/actions/user.js
@@ -1,20 +1,15 @@
 import { doc, setDoc, getDoc, collection } from "firebase/firestore/lite";
 import { db } from "../../setup/firbase";
-
 export const getUser = (id) => async (dispatch) => {
-  console.log(id);
-  const userRef = collection(db, `users`);
-  getDoc(userRef).then((callref) => {
-    console.log(callref);
-  });
-
-  // dispatch({ type: "SET_USER", payload: user });
+  const userinfoRef = doc(db, "users", `${id}/info/userDetails`);
+  const userInfo = await getDoc(userinfoRef);
+  dispatch({ type: "SET_USER", payload: userInfo.data() });
 };
 
 export const createDBUser = (userDetails, id) => async (dispatch) => {
-  const usersRef = doc(db, `users/${id}`);
-  await setDoc(usersRef, userDetails);
-  dispatch({ type: "SET_USER", payload: userDetails });
+  const usersRef = collection(db, `users/${id}/info`);
+  const userinfoRef = doc(usersRef, "userDetails");
+  await setDoc(userinfoRef, userDetails);
 };
 
 export const Logout = () => async (dispatch) => {

3ef133d789f0099659f3c410ff036e275982518f
eid sid <eidsiddiedis@gmail.com>
2022-03-23 19:33:13 +0200

    update baskt item count in Baskt component

--- src/setup/reducers/Baskt.js
+++ src/setup/reducers/Baskt.js
@@ -5,9 +5,11 @@ export default (Baskt = [], action) => {
     case "ADD_ITEM":
       return [...Baskt, action.payload];
     case "REMOVE_ITEM":
-      console.log(action.payload);
       return Baskt.filter((item) => item.id !== action.payload);
-
+    case "UPDATE_ITEM":
+      return Baskt.map((item) =>
+        item.id == action.payload.id ? action.payload : item
+      );
     default:
       return Baskt;
   }

27efd2033c775d839ecc302ce89faea4098824dd
eid sid <eidsiddiedis@gmail.com>
2022-10-26 16:11:35 +0200

    improve code

--- src/component/register/Register.js
+++ src/component/register/Register.js
@@ -8,43 +8,61 @@ import { auth, createUser } from "setup/firbase";
 import { createDBUser } from "setup/actions/user";
 
 const Register = ({ user }) => {
-  const [userName, setuserName] = useState("");
-  const [userEmail, setuserEmail] = useState("");
-  const [Password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const [formData, setFormData] = useState({
+    Name: "",
+    Email: "",
+    Password: "",
+  });
+  function handleFormData(event) {
+    const { name, value } = event.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
   const [isprocess, setisprocess] = useState(false);
   const [Errors, setErrors] = useState("");
   const [userPass, setUserPass] = useState(true);
   const navigate = useNavigate();
   if (user) navigate("/");
-  const dispatch = useDispatch();
+
   function checkEmail() {
-    if (userName === "" || userEmail === "" || Password == "") {
+    if (
+      formData.Name === "" ||
+      formData.Email === "" ||
+      formData.Password == ""
+    ) {
       setErrors("All Field must be filed");
       setUserPass(false);
     }
 
-    if (userName < 3) {
+    if (formData.Name < 3) {
       setUserPass(false);
       setErrors("Your Name must be more than 2 character");
     }
-    if (Password.length < 8) {
+    if (formData.Password.length < 8) {
       setUserPass(false);
       setErrors("Password must be more than 7 character");
     }
-    if (userEmail.length > 3) {
+    if (formData.Email.length > 3) {
       setUserPass(false);
       setErrors("Email must be more than 3 character");
     }
   }
+
   const Register = async (e) => {
     e.preventDefault();
     checkEmail();
     if (userPass) {
       setisprocess(true);
-      await createUser(auth, userEmail, Password)
+      await createUser(auth, formData.Email, formData.Password)
         .then(async (data) => {
           if (data) {
-            let userDetails = { name: userName, email: data.user.email };
+            let userDetails = {
+              name: formData.Name,
+              email: data.user.email,
+            };
 
             dispatch(createDBUser(userDetails, data.user.uid));
           }
@@ -73,20 +91,15 @@ const Register = ({ user }) => {
         <h2>Register</h2>
         <div className="form__control">
           <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            onChange={(e) => setuserName(e.target.value)}
-          />
+          <input type="text" name="Name" id="name" onChange={handleFormData} />
         </div>
         <div className="form__control">
           <label htmlFor="email">E-mail</label>
           <input
             type="email"
-            name="email"
+            name="Email"
             id="email"
-            onChange={(e) => setuserEmail(e.target.value)}
+            onChange={handleFormData}
           />
         </div>
 
@@ -94,12 +107,21 @@ const Register = ({ user }) => {
           <label htmlFor="password">Password</label>
           <input
             type="password"
-            name="password"
+            name="Password"
             id="password"
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={handleFormData}
           />
         </div>
-        <button disabled={isprocess}>Register</button>
+        <button
+          disabled={
+            isprocess ||
+            !formData.Email.length ||
+            !formData.Password.length ||
+            !formData.Name.length
+          }
+        >
+          Register
+        </button>
       </form>
       <div className="addition">
         <p>have an account ?</p>

4342a7e8c23fee7e3beb239be1345a3a323d4c80
eidsid <eidsiddiedis@gmail.com>
2023-09-22 18:46:54 +0200

    update

--- src/component/header/AccountDropdown/AccountDropdown.js
+++ src/component/header/AccountDropdown/AccountDropdown.js
@@ -1,27 +1,53 @@
 import React, { useState } from "react";
+import ArrowDropDown from "@mui/icons-material/ArrowDropDown";
 import "./style.scss";
-const AccountDropdown = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
-
+import { Link } from "react-router-dom";
+const AccountDropdown = ({ signOut }) => {
   return (
     <div className="account-dropdown">
-      <button className="account-dropdown-button" onClick={toggleDropdown}>
-        Your Account
-      </button>
-      {isOpen && (
-        <div className="account-dropdown-menu">
-          <ul>
-            <li>Your Account</li>
-            <li>Orders</li>
-            <li>Order</li>
-            <li>Sign Out</li>
-          </ul>
+      <div className="account-dropdown-title">
+        <span>Your Account</span>
+        <ArrowDropDown className="icon" />
+      </div>
+
+      <div className="account-dropdown-menu">
+        {" "}
+        <div className="top">
+          <Link to={"/login"} className="btn">
+            <span> Sign in</span>
+          </Link>
+          <div className="register">
+            New cusomer?
+            <Link to={"/register"} className="startLink">
+              Start here.
+            </Link>
+          </div>
         </div>
-      )}
+        <hr />
+        <div className="bottom">
+          <ul className="left">
+            <li>
+              <Link to={"/account"}>Your Account</Link>
+            </li>
+            <li>
+              <Link to={"/orders"}>Orders</Link>
+            </li>
+            <li>
+              <Link to={"/"}>Recommendations</Link>
+            </li>
+            <li onClick={() => signOut()}>Sign Out</li>
+          </ul>
+
+          <div className="center"></div>
+
+          <div className="right">
+            <h3>Your Lists</h3>
+            <p>Create a List</p>
+            <p>Find a List or Registry</p>
+          </div>
+          <div className="account-dropdown-menu-layer"></div>
+        </div>
+      </div>
     </div>
   );
 };

fa72b57d6ad3bc8a31f046558bd9c87387f2a200
eidsid <eidsiddiedis@gmail.com>
2023-09-25 06:13:48 +0200

    create account page

--- src/pages/Account/AccountSetting.js
+++ src/pages/Account/AccountSetting.js
@@ -1,16 +1,29 @@
+/* eslint-disable no-restricted-globals */
+
 import React from "react";
 import "./style.scss";
+import { useSelector } from "react-redux";
+import { auth } from "setup/firbase";
 
+function DeleteUserAccount() {
+  const user = auth.currentUser;
+  const conformDelete = confirm("Are you sure you want to delete your account");
+  if (conformDelete) user.delete();
+}
 const AccountSetting = () => {
+  const User = useSelector((state) => state.user);
+  console.log(User, auth.currentUser);
   return (
     <div className="account-page">
-      <h1>Account Page</h1>
+      <h1></h1>
       <p>Welcome to your Amazon account!</p>
       <div className="account-details">
-        <p>Name: John Doe</p>
-        <p>Email: johndoe@example.com</p>
-        {/* Add more account details */}
+        <p>Name: {User.name} </p>
+        <p>Email: {User.email} </p>
       </div>
+      <button className=" delete-button" onClick={() => DeleteUserAccount()}>
+        Delete
+      </button>
     </div>
   );
 };

cea262297db594ad0e480e4ab5accd87983b17d5
eidsid <eidsiddiedis@gmail.com>
2023-09-28 20:22:33 +0200

    improve code

--- src/setup/actions/Baskt.js
+++ src/setup/actions/Baskt.js
@@ -9,11 +9,13 @@ import {
 } from "firebase/firestore/lite";
 import { db } from "../../setup/firbase";
 
+const itemCollectionRef = (user_id) => collection(db, `users/${user_id}/Baskt`);
+
 export const getItems = (id) => async (dispatch) => {
   try {
     let data = [];
-    const userinfoRef = collection(db, `users/${id}/Baskt`);
-    await getDocs(userinfoRef).then((docs) => {
+    const BasktRef = itemCollectionRef(id);
+    await getDocs(BasktRef).then((docs) => {
       docs.forEach((doc) => {
         data.push(doc.data());
       });
@@ -23,19 +25,14 @@ export const getItems = (id) => async (dispatch) => {
     console.log(error.message);
   }
 };
+
 export const ADD_ITEM = (userID, item) => async (dispatch) => {
   try {
-    const BasktRef = collection(db, `users/${userID}/Baskt`);
+    const BasktRef = itemCollectionRef(userID);
     const itemRef = doc(BasktRef, `${item.id}`);
     const snapDoc = await getDoc(itemRef);
-
     if (snapDoc.exists()) {
-      let object = snapDoc.data();
-      let count = object.count + 1;
-      let updatedItem = { ...object, count };
-      await updateDoc(itemRef, updatedItem).then(() => {
-        dispatch({ type: "UPDATE_ITEM", payload: updatedItem });
-      });
+      UPDATE_ITEM_COUNT(userID, item.id, 1);
     } else {
       let newItem = { ...item, count: 1 };
       setDoc(itemRef, newItem).then(() => {
@@ -46,10 +43,27 @@ export const ADD_ITEM = (userID, item) => async (dispatch) => {
     console.log(error.message);
   }
 };
+export const UPDATE_ITEM_COUNT = (userID, id, number) => async (dispatch) => {
+  try {
+    const BasktRef = itemCollectionRef(userID);
+    const itemRef = doc(BasktRef, `${id}`);
+    const snapDoc = await getDoc(itemRef);
+    let object = snapDoc.data();
+
+    let count = object.count + number;
+    if (count > 0) {
+      let updatedItem = { ...object, count };
+      await updateDoc(itemRef, updatedItem).then(() => {
+        dispatch({ type: "UPDATE_ITEM", payload: updatedItem });
+      });
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 export const REMOVE_ITEM = (userID, id) => async (dispatch) => {
   try {
-    console.log(id);
-    const BakstRef = collection(db, `users/${userID}/Baskt`);
+    const BakstRef = itemCollectionRef(userID);
     const itemRef = doc(BakstRef, `${id}`);
     await deleteDoc(itemRef).then(() => {
       dispatch({ type: "REMOVE_ITEM", payload: id });

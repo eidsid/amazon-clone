b5f789b691dbaad22576f44a549956b916210497
Eid Sayed <eidsiddiedis@gmail.com>
2023-10-08 16:52:22 +0200

    add loding icon

--- src/pages/Account/AccountSetting.js
+++ src/pages/Account/AccountSetting.js
@@ -1,9 +1,10 @@
 /* eslint-disable no-restricted-globals */
 import React from "react";
 import "./style.scss";
-import { useNavigate } from "react-router-dom";
 import { auth } from "setup/firbase";
 import { useSelector } from "react-redux";
+import useLoginRedirect from "hooks/useLoginRedirect";
+import Loading from "component/loadingIcon/Loading";
 
 function DeleteUserAccount() {
   const user = auth.currentUser;
@@ -12,24 +13,31 @@ function DeleteUserAccount() {
 }
 
 const AccountSetting = () => {
-  const User = useSelector(state => state.user);
-  const navigate = useNavigate();
+  const User = useSelector((state) => state.user);
 
-  if (!User) navigate("/login");
+  useLoginRedirect(User);
 
   return (
-    <div className="account-page">
-      <h1></h1>
-      <p>Welcome to your Amazon account!</p>
-      <div className="account-details">
-        <p>Name: {User?.name} </p>
-        <p>Email: {User?.email} </p>
-      </div>
-      <button className=" delete-button" onClick={() => DeleteUserAccount()}>
-        Delete
-      </button>
-    </div>
+    <>
+      {User ? (
+        <div className="account-page">
+          <h1></h1>
+          <p>Welcome to your Amazon account!</p>
+          <div className="account-details">
+            <p>Name: {User?.name} </p>
+            <p>Email: {User?.email} </p>
+          </div>
+          <button
+            className=" delete-button"
+            onClick={() => DeleteUserAccount()}
+          >
+            Delete
+          </button>
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </>
   );
 };
-
 export default AccountSetting;

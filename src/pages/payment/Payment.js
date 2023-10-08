4ccd7174c2ee8d7dca240cec6e3171f49a295475
Eid Sayed <eidsiddiedis@gmail.com>
2023-10-08 12:30:41 +0200

    restyle this component

--- src/pages/payment/Payment.js
+++ src/pages/payment/Payment.js
@@ -1,143 +1,149 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
-import "./style.scss";
 import axios from "axios";
 import { AddOrder } from "setup/actions/orders";
 import { AddNotifications } from "setup/actions/notification";
 import { getAllCost } from "hooks/CalcTotalCost";
+import "./style.scss";
+import useLoginRedirect from "hooks/useLoginRedirect";
+import Loading from "component/loadingIcon/Loading";
+import CountryList from "component/countryList/CountryList";
 
 const Payment = () => {
   const user = useSelector((state) => state.user);
   const navigate = useNavigate();
   const dispatch = useDispatch();
-
   const Baskt = useSelector((state) => state.Baskt);
-  const [products, setproducts] = useState(Baskt);
+  const [products, setProducts] = useState(Baskt);
 
   const removeItem = (e, id) => {
     e.target.parentElement.parentElement.classList.add("close");
-   
-      dispatch(
-        AddNotifications({ msg: "item removed success ", type: "success" })
-      );
-     
-      setproducts((products) => products.filter((item) => item.id !== id));
-    
-  };
-  const itemsDom = products.map((item) => {
-    return (
-      <div className="product" key={item.id}>
-        <Link to={`/info/${item.id}`} className="linke">
-          <img src={item.image} alt="image" className="image" />
-        </Link>
-        <div className="info">
-          <p> {item.title} </p>
-          <p>{item.count} in stack</p>
-          <p>Eligible for FREE delivery</p>
-          <button onClick={(e) => removeItem(e, item.id)}> remove item </button>
-          <p className="price">
-            <strong> $ {item.price * item.count} </strong>
-          </p>
-        </div>
-      </div>
+    dispatch(
+      AddNotifications({ msg: "item removed success ", type: "success" })
     );
-  });
+    setProducts((prevProducts) =>
+      prevProducts.filter((item) => item.id !== id)
+    );
+  };
 
-  const [Carderrors, setCarderrors] = useState("");
-  const [disabled, setdisabled] = useState(true);
-  const [processing, setprocessing] = useState("");
-  const [succeeded, setsucceeded] = useState(false);
+  const itemsDom = products.map((item) => (
+    <div className="product" key={item.id}>
+      <Link to={`/info/${item.id}`} className="linke">
+        <img src={item.image} alt="image" className="image" />
+      </Link>
+      <div className="info">
+        <p>{item.title}</p>
+        <p>{item.count} in stock</p>
+        <p>Eligible for FREE delivery</p>
+        <button onClick={(e) => removeItem(e, item.id)}>remove item</button>
+        <p className="price">
+          <strong>${item.price * item.count}</strong>
+        </p>
+      </div>
+    </div>
+  ));
+
+  const [cardErrors, setCardErrors] = useState("");
+  const [disabled, setDisabled] = useState(true);
+  const [processing, setProcessing] = useState("");
+  const [succeeded, setSucceeded] = useState(false);
+  const [Country, setCountry] = useState("");
   const stripe = useStripe();
   const elements = useElements();
-  let TotalCost=getAllCost(products);
-  useEffect(() => {
-     TotalCost=getAllCost(products);
-  }, [products.length])
-  const handelChange = (e) => {
-    setCarderrors(e.error ? e.error.message : "");
-    setdisabled(!e.empity & e.complete & !Carderrors ? false : true);
+  let totalCost = getAllCost(products);
+
+  const handleChange = (e) => {
+    setCardErrors(e.error ? e.error.message : "");
+    setDisabled(!e.empty && e.complete && !cardErrors ? false : true);
   };
 
-  const handelSubmit = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    setprocessing(true);
-    //  generate the epecial stripe secret which alows us to charge acustomer
-    await axios
-      .post("https://amazone-clone-server.onrender.com/payments/create", {
-        total: Math.floor(TotalCost * 1),
-      })
-      .then(async (data) => {
-        await stripe
-          .confirmCardPayment(data.data.clientSecret, {
-            payment_method: {
-              card: elements.getElement(CardElement),
-            },
-          })
-          .then(async ({ paymentIntent }) => {
-            AddOrder(user.userID, {
-              products,
-              paymentID: paymentIntent.id,
-              created: paymentIntent.created,
-              Amount: paymentIntent.amount,
-            });
+    setProcessing(true);
 
-            setsucceeded(true);
-            setprocessing(null);
-            setCarderrors(null);
-            navigate("/orders");
-          });
+    try {
+      const response = await axios.post(
+        "https://amazone-clone-server.onrender.com/payments/create",
+        {
+          total: Math.floor(totalCost * 1),
+        }
+      );
+      const { data } = response;
+
+      const { clientSecret } = data;
+      const { paymentIntent } = await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+        },
       });
+
+      AddOrder(user.userID, {
+        products,
+        paymentID: paymentIntent.id,
+        created: paymentIntent.created,
+        Amount: paymentIntent.amount,
+        Country,
+      });
+
+      setSucceeded(true);
+      setProcessing(null);
+      setCardErrors(null);
+      navigate("/orders");
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-    if (!user) {
-        dispatch(
-          AddNotifications({ msg: "you shoud login frist", type: "error" })
-        );
-        navigate("/login"); 
-        }
+  useLoginRedirect(user);
+  useEffect(() => {
+    totalCost = getAllCost(products);
+  }, [products.length]);
 
+  useEffect(() => {
+    setProducts(Baskt);
+  }, [Baskt]);
+  function CountryFun(value) {
+    setCountry(value);
+  }
   return (
     <>
       <div className="checkCount">
         <h1>
-          Checkout ( <span>{products.length} items</span> )
+          Checkout (<span>{products.length} items</span>)
         </h1>
       </div>
       <div className="paymentContainer">
         <div className="address__info">
-          <span>Delivery Address</span>
-          <div>
-            <p>{user ? user.email : ""}</p>
-            <p>adress1</p>
-            <p>adress2</p>
-          </div>
+          <span>Delivery Address information</span>
+          <p>Email: {user ? user.email : ""}</p>
+          <h3>Country: {Country}</h3>
+          Change Country <CountryList setCountryFun={CountryFun} />
         </div>
         <hr />
         <div className="productsSection">
           <span>Review items and delivery</span>
           <div className="products">
-            {products.length ? itemsDom : <h1>no items</h1>}
+            {products.length ? itemsDom : <Loading />}
           </div>
         </div>
         <hr />
-        <div className="paymentMothed">
-          <span>paymentMothed</span>
+        <div className="paymentMethod">
+          <span>Payment Method</span>
           <div>
             <span>Card Details</span>
             <div>
               <div className="cardInfo">
-                <form onSubmit={handelSubmit}>
-                  <CardElement onChange={handelChange} />
-                  <p className="errors">{Carderrors}</p>
+                <form onSubmit={handleSubmit}>
+                  <CardElement onChange={handleChange} />
+                  <p className="errors">{cardErrors}</p>
                   <div className="order">
-                    <span>Order Total: ${TotalCost}</span>
+                    <span>Order Total: ${totalCost}</span>
                     <button disabled={disabled || processing || succeeded}>
                       <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
                     </button>
-                  </div>{" "}
+                  </div>
                 </form>
               </div>
             </div>

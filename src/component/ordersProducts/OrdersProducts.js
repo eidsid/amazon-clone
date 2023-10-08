6796232ad5f8509fc1b309b1d8920d8d52efa37a
Eid Sayed <eidsiddiedis@gmail.com>
2023-10-08 17:04:30 +0200

    edit code and add adress

--- src/component/ordersProducts/OrdersProducts.js
+++ src/component/ordersProducts/OrdersProducts.js
@@ -1,6 +1,6 @@
 import SingleOrderProductDom from "component/SingleOrderProductDom/SingleOrderProductDom";
 import "./style.scss";
-const OrdersProducts = (props) => {
+const OrdersProducts = ({ adress, id, amount, date, products }) => {
   return (
     <div className="orders">
       <div className="info">
@@ -9,18 +9,19 @@ const OrdersProducts = (props) => {
             <strong>order</strong>
           </div>
           <div>
-            <small>paymentID: {props.id}</small>
+            <small>paymentID: {id}</small>
           </div>
         </div>
         <div className="col">
-          <div>{props.date}</div>
+          <div>{date}</div>
           <div>
-            <strong>Total ${props.amount}</strong>
+            <strong>Total ${amount}</strong>
+            {adress && <span>deleverd to {adress}</span>}
           </div>
         </div>
       </div>
       <div className="products">
-        {props.products.map((item, index) => {
+        {products.map((item, index) => {
           <SingleOrderProductDom item={item} key={index} />;
         })}
       </div>
